6d079443fcd6b5cbd9e85d89184909fc8cc2e6f7
Henry Mitchell <henry@henrymitchell.org>
2019-08-11 19:47:39 -0400

    Made agenda files work again.

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
     ("26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de" default)))
  '(org-agenda-files
    (quote
-    ("/Users/hmmitche/Todo/school/misc.org" "/Users/hmmitche/Todo/school/thesis.org" "/Users/hmmitche/Todo/life/list.org")))
+    ("/Users/hmmitche/Todo/school/" "/Users/hmmitche/Todo/life/")))
  '(package-selected-packages
    (quote
     (evil-indent-plus evil-fringe-mark helm pdf-tools evil-magit magit julia-mode magic-latex-buffer xml+ evil-goggles which-key use-package rainbow-delimiters py-autopep8 markdown-mode jedi highlight-symbol gruvbox-theme general exec-path-from-shell evil-surround evil-leader evil-easymotion evil-commentary elpy dimmer counsel browse-kill-ring auctex)))
